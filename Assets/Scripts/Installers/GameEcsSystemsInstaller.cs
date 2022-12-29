d884f501941bb0e291aff0ab4360086edb41fd0a
Kulychevs <s.kulychev@gmail.com>
2022-12-29 18:15:23 +0300

    register systems

--- Assets/Scripts/Installers/GameEcsSystemsInstaller.cs
+++ Assets/Scripts/Installers/GameEcsSystemsInstaller.cs
@@ -1,7 +1,4 @@
 using Systems;
-using Leopotam.EcsLite.UnityEditor;
-using UnityEngine.PlayerLoop;
-using UnityTemplateProjects.Systems;
 
 namespace UnityTemplateProjects.Installers
 {
@@ -9,7 +6,21 @@ namespace UnityTemplateProjects.Installers
     {
         protected override void OnInstallBindings()
         {
-            Container.BindSystem<SomeValueShowSystem, UpdateSystemsContainer>();
+            Container.BindSystem<PlayerInputSystem, UpdateSystemsContainer>();
+
+            Container.BindSystem<ButtonActivationCheckSystem, UpdateSystemsContainer>();
+            Container.BindSystem<ButtonActivationSystem, UpdateSystemsContainer>();
+            Container.BindSystem<ButtonDectivationSystem, UpdateSystemsContainer>();
+
+            Container.BindSystem<DoorActivationSystem, UpdateSystemsContainer>();
+            Container.BindSystem<DoorDeactivationSystem, UpdateSystemsContainer>();
+
+            Container.BindSystem<MovementSystem, UpdateSystemsContainer>();
+            Container.BindSystem<RotationSystem, UpdateSystemsContainer>();
+
+            Container.BindSystem<SynchronizeTransformSystem, UpdateSystemsContainer>();
+            Container.BindSystem<PlayerAnimationSystem, UpdateSystemsContainer>();
+            Container.BindSystem<SynchronizeCameraSystem, LateUpdateSystemsContainer>();
         }
     }
 }
